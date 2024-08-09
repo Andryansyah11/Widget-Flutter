0a051e3b6d7ebc5dc53747c89785e5937807981f
Andryansyah11 <andryansyah.2912@gmail.com>
2024-08-09 19:09:48 +0700

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:baru/Scafold.dart';
-import 'package:baru/button.dart';
-import 'package:baru/cupertino.dart';
+import 'package:baru/Scaffold.dart';
+import 'package:baru/tugas.dart';
+// import 'package:baru/button.dart';
+// import 'package:baru/cupertino.dart';
 // import 'package:baru/image_widget.dart';
 // import 'package:baru/text_widget.dart';
 import 'package:flutter/material.dart';
@@ -16,10 +17,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return  const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: ScaffoldScreen(),
+      home: MyTugas(),
     );
   }
 }
